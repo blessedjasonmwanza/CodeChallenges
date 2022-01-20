c9d802cded1a52e6bb6b3512c2d2472dd7036e29
blessedjasonmwanza <mwanzabj@gmail.com>
2022-01-20 10:05:47 +0200

    Add migratory birds challenge

---
+++ migratorybirds.js
@@ -0,0 +1,25 @@
+// ! NEEDS error fixing to pass all test
+
+/**
+ * CHALLENGE ADAPTED FROM hackerank
+ * @param {} arr 
+ * @returns 
+ */
+function migratoryBirds(arr) {
+    // Write your code here
+    const sorted = Array.from(new Set(arr)).sort((a, b) => (a - b));
+    const largest_2 = sorted.slice(-2);
+    // return largest_2;
+    const first = arr.filter((value) => value === largest_2[0]).length;
+    return first;
+    const second = arr.filter((value) => value === largest_2[1]).length;
+    if(first === second){
+        return largest_2[0];
+    }else{
+        return first > second ? largest_2[0] : largest_2[1] ;
+    }
+
+}
+// migratoryBirds([1, 4, 4, 4, 5, 3]);
+
+console.log(migratoryBirds([1, 4, 4, 4, 5, 3]))
